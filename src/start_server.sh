f43ea27be3a7143c2513bc59f4610f60c9744b41
LucaMaro1 <49284160+LucaMaro1@users.noreply.github.com>
2021-12-23 13:37:33 +0100

    Delete start_server.sh

--- src/start_server.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd ./server
-javac Card.java MainServer.java NotifyEventInterface.java Progetto.java ServerInterface.java Server.java Utente.java -classpath ../lib/com.fasterxml.jackson/jar_files/jackson-core-2.12.0.jar:../lib/com.fasterxml.jackson/jar_files/jackson-databind-2.12.0.jar:../lib/com.fasterxml.jackson/jar_files/jackson-annotations-2.12.0.jar
-if [[ $? -eq 0 ]]; then
-	echo "Compilazione server eseguita."
-	echo ""
-	java -classpath .:../lib/com.fasterxml.jackson/jar_files/jackson-core-2.12.0.jar:../lib/com.fasterxml.jackson/jar_files/jackson-databind-2.12.0.jar:../lib/com.fasterxml.jackson/jar_files/jackson-annotations-2.12.0.jar MainServer
-fi
